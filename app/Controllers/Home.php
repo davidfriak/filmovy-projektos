884d78a507ec73e8d258be7cf131df510fa3ff04
BenVrahcz <davidfriak@email.cz>
2026-06-08 10:34:21 +0200

    commit

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,30 @@
 
 namespace App\Controllers;
 
+use App\Models\MovieData;
+use App\Models\ActorData;
+use App\Models\Genre;
+
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
-        return view('welcome_message');
+        $movieModel = new MovieData();
+        $actorModel = new ActorData();
+        $genreModel = new Genre();
+
+        $data['movieCount'] = $movieModel->countAll();
+        $data['actorCount'] = $actorModel->countAll();
+        $data['genreCount'] = $genreModel->countAll();
+
+        $data['latestMovies'] = $movieModel
+            ->orderBy('pid_movie', 'DESC')
+            ->findAll(8);
+
+        $data['latestActors'] = $actorModel
+            ->orderBy('pid_actor', 'DESC')
+            ->findAll(5);
+
+        return view('welcome_message', $data);
     }
 }
